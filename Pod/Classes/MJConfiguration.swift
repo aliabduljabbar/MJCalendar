97592e7b57e9614d0bd7b73fe29a16e274491e28
Ali Abdul Jabbar <ali@bargoventures.com>
2018-05-29 13:34:33 +0500

    exposing configures attributes to objective C interface

--- Pod/Classes/MJConfiguration.swift
+++ Pod/Classes/MJConfiguration.swift
@@ -35,7 +35,7 @@ import Foundation
         case three
     }
 
-    public enum SelectedDayType {
+    @objc public enum SelectedDayType: Int {
         case filled, border
     }
 
@@ -44,35 +44,35 @@ import Foundation
     public var periodType: PeriodType = .month
     public var dayViewType: DayViewType = .circle
     public var startDayType: StartDayType = .monday
-    public var selectedDayType: SelectedDayType = .border
+    @objc public var selectedDayType: SelectedDayType = .border
     
-    public var rowHeight: CGFloat = 30
-    public var dayViewSize: CGSize = CGSize(width: 24, height: 24)
-    public var dayTextFont = UIFont.systemFont(ofSize: 12)
+    @objc public var rowHeight: CGFloat = 30
+    @objc public var dayViewSize: CGSize = CGSize(width: 24, height: 24)
+    @objc public var dayTextFont = UIFont.systemFont(ofSize: 12)
     
-    public var otherMonthBackgroundColor = UIColor.clear
-    public var otherMonthDayViewBackgroundColor = UIColor.clear
-    public var otherMonthTextColor = UIColor.clear
+    @objc public var otherMonthBackgroundColor = UIColor.clear
+    @objc public var otherMonthDayViewBackgroundColor = UIColor.clear
+    @objc public var otherMonthTextColor = UIColor.clear
     
-    public var dayBackgroundColor = UIColor.clear
-    public var dayDayViewBackgroundColor = UIColor.clear
-    public var dayTextColor = UIColor.clear
+    @objc public var dayBackgroundColor = UIColor.clear
+    @objc public var dayDayViewBackgroundColor = UIColor.clear
+    @objc public var dayTextColor = UIColor.clear
     
-    public var selectedDayBackgroundColor = UIColor.clear
-    public var selectedDayTextColor = UIColor.clear
-    public var selectedBorderWidth: CGFloat = 1
+    @objc public var selectedDayBackgroundColor = UIColor.clear
+    @objc public var selectedDayTextColor = UIColor.clear
+    @objc public var selectedBorderWidth: CGFloat = 1
     
-    public var weekLabelFont = UIFont.systemFont(ofSize: 12)
-    public var weekLabelTextColor = UIColor.clear
-    public var weekLabelHeight: CGFloat = 25
+    @objc public var weekLabelFont = UIFont.systemFont(ofSize: 12)
+    @objc public var weekLabelTextColor = UIColor.clear
+    @objc public var weekLabelHeight: CGFloat = 25
     
-    public var minDate: Date?
-    public var maxDate: Date?
+    @objc public var minDate: Date?
+    @objc public var maxDate: Date?
     
-    public var outOfRangeDayBackgroundColor = UIColor.clear
-    public var outOfRangeDayTextColor = UIColor.clear
+    @objc public var outOfRangeDayBackgroundColor = UIColor.clear
+    @objc public var outOfRangeDayTextColor = UIColor.clear
     
-    public var selectDayOnPeriodChange: Bool = true
+    @objc public var selectDayOnPeriodChange: Bool = true
     
     static func getDefault() -> MJConfiguration {
         let configuration = MJConfiguration()
